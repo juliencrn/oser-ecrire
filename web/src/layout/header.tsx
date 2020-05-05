026cccdec9c11429d66ff8b0f06b8a2952b9e074
Julien <juliencaron@protonmail.com>
2020-05-05 21:47:38 +0200

    Menu mobile

--- web/src/layout/header.tsx
+++ web/src/layout/header.tsx
@@ -1,21 +1,18 @@
 import { Link as GatsbyLink } from 'gatsby'
 import React, { FC } from 'react'
-import { makeStyles, Theme, useTheme } from '@material-ui/core/styles'
+import { makeStyles, Theme } from '@material-ui/core/styles'
 import Headroom from 'react-headroom'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
+
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
-import Button from '@material-ui/core/Button'
 import Divider from '@material-ui/core/Divider'
 import Box from '@material-ui/core/Box'
 import Hidden from '@material-ui/core/Hidden'
-import IconButton from '@material-ui/core/IconButton'
-
-import MenuIcon from '@material-ui/icons/Menu'
 
 import { routes } from '../config'
+import Menu from './menu'
 
 const useStyles = makeStyles((theme: Theme) => ({
   appBar: {
@@ -44,17 +41,8 @@ export interface HeaderProps {
   isBlog?: boolean
 }
 
-const links = [
-  { label: 'À propos', to: '/a-propos' },
-  { label: "L'atelier d'écriture", to: routes.blog },
-  { label: 'Rédaction SEO', to: routes.redac },
-  { label: 'Contact', to: '/contact' },
-]
-
 const Header: FC<HeaderProps> = ({ siteTitle, isBlog = false }) => {
   const classes = useStyles()
-  const theme = useTheme()
-  const isLarge = useMediaQuery(theme.breakpoints.up('md'))
 
   return (
     <Headroom>
@@ -94,19 +82,7 @@ const Header: FC<HeaderProps> = ({ siteTitle, isBlog = false }) => {
             </Hidden>
           </div>
 
-          {isLarge ? (
-            <>
-              {links.map(({ label, to }) => (
-                <Button key={to} component={GatsbyLink} to={to} color="inherit">
-                  {label}
-                </Button>
-              ))}
-            </>
-          ) : (
-            <IconButton>
-              <MenuIcon />
-            </IconButton>
-          )}
+          <Menu />
         </Toolbar>
       </AppBar>
     </Headroom>
---
+++ web/src/layout/menu.tsx
@@ -0,0 +1,98 @@
+import React, { FC } from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import { useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import Drawer from '@material-ui/core/Drawer'
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+
+import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close'
+
+import { routes } from '../config'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  drawerHeader: {
+    ...theme.mixins.toolbar,
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+}))
+
+export interface MenuItem {
+  label: string
+  to: string
+}
+
+export interface MenuProps {
+  links: MenuItem[]
+  isLarge: boolean
+}
+
+const links: MenuItem[] = [
+  { label: 'À propos', to: '/a-propos' },
+  { label: "L'atelier d'écriture", to: routes.blog },
+  { label: 'Rédaction SEO', to: routes.redac },
+  { label: 'Contact', to: '/contact' },
+]
+
+const MenuLinks: FC<MenuProps> = ({ links, isLarge }) => (
+  <>
+    {links.map(({ label, to }) => (
+      <Button
+        key={to}
+        component={GatsbyLink}
+        to={to}
+        size={isLarge ? `medium` : `large`}
+        color="inherit"
+      >
+        {label}
+      </Button>
+    ))}
+  </>
+)
+
+const Menu: FC = () => {
+  const theme = useTheme()
+  const classes = useStyles()
+  const isLarge = useMediaQuery(theme.breakpoints.up('lg'))
+  const [isOpen, setOpen] = React.useState(false)
+
+  const handleOpenMenu = () => {
+    setOpen(true)
+  }
+
+  const handleCloseMenu = () => {
+    setOpen(false)
+  }
+  return (
+    <>
+      {isLarge ? (
+        <MenuLinks links={links} isLarge={isLarge} />
+      ) : (
+        <>
+          <IconButton onClick={handleOpenMenu}>
+            <MenuIcon />
+          </IconButton>
+          <Drawer anchor="top" open={isOpen} onClose={handleCloseMenu}>
+            <Container maxWidth="lg" className={classes.drawerHeader}>
+              <IconButton onClick={handleCloseMenu}>
+                <CloseIcon />
+              </IconButton>
+            </Container>
+            <Box pb={1} px={2} display="flex" flexDirection="column">
+              <MenuLinks links={links} isLarge={isLarge} />
+            </Box>
+          </Drawer>
+        </>
+      )}
+    </>
+  )
+}
+
+export default Menu
