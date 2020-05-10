927d860a110b2ea014c19dbe218f3cee64fa97f2
Julien <juliencaron@protonmail.com>
2020-05-11 01:20:22 +0200

    studio/ Add admin email in settings field

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -11,6 +11,13 @@ export default {
       validation: (Rule) => Rule.error('Champ obligatoire').required(),
     },
     {
+      name: 'email',
+      type: 'string',
+      title: 'Email',
+      description: `Ceci est l'email administrateur, celui sur lequel vous recevrez les notifications de gestion du site.`,
+      validation: (Rule) => Rule.error('Champ obligatoire').required(),
+    },
+    {
       name: 'slogan',
       type: 'text',
       title: 'Slogan',
