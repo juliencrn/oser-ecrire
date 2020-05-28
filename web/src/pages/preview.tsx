00c718870f24400fb5e045242f99d7d27a515f2a
Julien <juliencaron@protonmail.com>
2020-05-28 04:11:44 +0200

    web/ Fix preview

--- web/src/pages/preview.tsx
+++ web/src/pages/preview.tsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import * as queryString from 'query-string'
 import { Helmet } from 'react-helmet'
-import { Match } from '@reach/router'
 import { navigate } from 'gatsby'
 
 import Layout from '../layout'
 import PostPreview from '../previews/PostPreview'
+import { PageTemplate } from '../interfaces'
 
-export default function PreviewPage() {
+export default function PreviewPage(props: PageTemplate) {
+  const parsed = queryString.parse(props.location.search)
+  const id = parsed?.id as string
+  const type = parsed?.type as string
+  let component = null
+
+  useEffect(() => {
+    if (!id || !type) {
+      navigate('/')
+    }
+  }, [])
+
+  switch (type) {
+    case 'post':
+      component = <PostPreview documentId={id} location={props.location} />
+      break
+    default:
+      return null
+  }
+
   return (
     <Layout>
       <Helmet>
         <meta name="robots" content="noindex, nofollow" />
       </Helmet>
 
-      <Match path="/preview">
-        {props => {
-          const parsed = queryString.parse(props.location.search)
-          const id = parsed?.id as string
-          const type = parsed?.type as string
-
-          if (!(id && props.match)) {
-            navigate('/')
-            return null
-          }
-
-          switch (type) {
-            case 'post':
-              return <PostPreview documentId={id} location={props.location} />
-
-            default:
-              navigate('/')
-              return null
-          }
-        }}
-      </Match>
+      {component}
     </Layout>
   )
 }
